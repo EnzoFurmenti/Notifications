56a8006c6aa983f9d0952af7d16c95f554c484d8
EnzoFurmenti <ducheenzo@mail.ru>
2016-08-02 08:04:46 +0300

    Initial Commit

---
+++ Notification/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  Notification
+//
+//  Created by EnzoF on 02.08.16.
+//  Copyright © 2016 EnzoF. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
